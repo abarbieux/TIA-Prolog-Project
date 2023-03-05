c46c27bdb25e8e8348f9f32e9371dd0694210d2e
abarbieux <arthur.barbieux@student.unamur.be>
2023-03-05 20:49:11 +0100

    cleaning code

--- Deck.gd
+++ Deck.gd
@@ -2,6 +2,7 @@ extends Object
 class_name Deck
 
 var _cards:Array = []
+var Deck_Carte_Player: Array
 const NumberOfValue:int = 12
 const MaxNumberOfValue:int = 8
 
@@ -15,3 +16,34 @@ func MakeDeck() -> void:
 	print(_cards)
 
 
+# Creat the deck of each participant
+func Init_deck() -> void :
+	var Deck:int
+	while Deck < 4:
+		Deck +=1
+		var Carte:int
+		var Carte_list:Array
+		while Carte < 5:
+			Carte+=1
+			var rng = randi() % _cards.size()
+			Carte_list.append(_cards[rng])
+			_cards.remove(rng)
+		Deck_Carte_Player.append(Carte_list)
+		
+func refile_deck(Index:int):
+	var Carte:int
+	var Carte_list:Array
+	while Carte < 5:
+		Carte+=1
+		var rng = randi() % _cards.size()
+		Carte_list.append(_cards[rng])
+		_cards.remove(rng)
+	Deck_Carte_Player[Index] = Carte_list
+	
+func Empty_Deck_check() -> void:
+	var i : int = 0
+	while i < Deck_Carte_Player.size():
+		if Deck_Carte_Player[i] == []:
+			refile_deck(i)
+			
+		i += 1
--- Main.gd
+++ Main.gd
@@ -8,17 +8,17 @@ const NumberOfTeamMember:int = 3
 var _Players:Array = []
 var _country_turn_index:int = 0 # team who begins
 var player_selected:Cycliste
-var Deck_list: Array
-var cards : Deck
+
+var _Deck : Deck
 signal Change_turn(Team)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	
-	cards = Deck.new()
-	cards.MakeDeck()	
-#	var rng = randi() % cards._cards.size()
+	_Deck = Deck.new()
+	_Deck.MakeDeck()	
+
 	
 	for country in Countries:
 		for PlayerNumber in range(1, NumberOfTeamMember + 1):
@@ -27,7 +27,7 @@ func _ready() -> void:
 	for player in _Players :
 		player.position = _path.curve.get_point_position(0)
 	
-	Init_deck()
+	_Deck.Init_deck()
 	Display_deck_button()
 
 func CreateNewPlayer(Pays:String, Numero:int):
@@ -38,28 +38,16 @@ func CreateNewPlayer(Pays:String, Numero:int):
 	$Player_Container.add_child(_player)
 	return _player
 
-# Creat the deck of each participant
-func Init_deck() -> void :
-	var Deck:int
-	while Deck < 4:
-		Deck +=1
-		var Carte:int
-		var Carte_list:Array
-		while Carte < 5:
-			Carte+=1
-			var rng = randi() % cards._cards.size()
-			Carte_list.append(cards._cards[rng])
-			cards._cards.remove(rng)
-		Deck_list.append(Carte_list)
+
 
 func Display_deck_button() -> void :
 	var i = 0
-	for carte in Deck_list[_country_turn_index]:
+	for carte in _Deck.Deck_Carte_Player[_country_turn_index]:
 		var button = Button.new()
 		button.text = str(carte)
 		button.rect_min_size.x = 96
 		button.icon = preload("res://icon.png")
-		$Bottom.add_child(button)
+		$Current_Cards.add_child(button)
 		var c = button.connect("pressed", self, "_button_pressed", [button, carte, i])
 		i += 1
 #		add_child(button)
@@ -72,11 +60,11 @@ func MovePlayer(value, index):
 	player_selected.CurrentCase = clamp(player_selected.CurrentCase, 0, _path.curve.get_point_count() - 1)
 	player_selected.position = _path.curve.get_point_position(player_selected.CurrentCase)
 
-	for child in $Bottom.get_children():
+	for child in $Current_Cards.get_children():
 		child.queue_free()
-	Deck_list[_country_turn_index].erase(value)
-	cards._cards.append(value)
-	Empty_Deck_check()
+	_Deck.Deck_Carte_Player[_country_turn_index].erase(value)
+	_Deck._cards.append(value)
+	_Deck.Empty_Deck_check()
 	
 	_country_turn_index += 1
 	if _country_turn_index > Countries.size() - 1:
@@ -84,15 +72,7 @@ func MovePlayer(value, index):
 	emit_signal("Change_turn",Countries[_country_turn_index])
 	Display_deck_button()
 
-func refile_deck(Index:int):
-	var Carte:int
-	var Carte_list:Array
-	while Carte < 5:
-		Carte+=1
-		var rng = randi() % cards._cards.size()
-		Carte_list.append(cards._cards[rng])
-		cards._cards.remove(rng)
-	Deck_list[Index] = Carte_list
+
 
 func choose_player(value, index):
 	for kids in $ChoosePlayer.get_children() :
@@ -115,10 +95,4 @@ func _button_player_pressed(player, value, index) -> void:
 	MovePlayer(value, index)
 #	emit_signal("Player_Chosen")
 	
-func Empty_Deck_check() -> void:
-	var i : int = 0
-	while i < Deck_list.size():
-		if Deck_list[i] == []:
-			refile_deck(i)
-			
-		i += 1
+
