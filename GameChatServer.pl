1c693479926471df8a8699d5a92906e0dc37484f
David5031 <david.tang@student.unamur.be>
2023-03-19 15:36:25 +0100

    delete useless comments + delete useless giles

--- GameChatServer.pl
+++ GameChatServer.pl
@@ -1,25 +1,3 @@
-/*:- use_module(library(http/websocket)).
-:- use_module(library(http/thread_httpd)).
-:- use_module(library(http/http_dispatch)).
-
-:- http_handler(root(ws),
-	http_upgrade_to_websocket(echo, []),
-	[spawn([])]).
-
-echo(WebSocket) :-
-    ws_receive(WebSocket, Message),
-    (Message.opcode == close
-    ->  true
-    ;   string_concat('Hey, you said ', Message.data , MessageRes),
-    	ws_send(WebSocket, text(MessageRes)),
-        echo(WebSocket)
-    ).
-
-:- initialization
-	http_server(http_dispatch, [port(5000)]).
-
-*/
-
 :- use_module(library(lists)).
 :- use_module(library(isub)).
 :- use_module(library(http/websocket)).
