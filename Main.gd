46c90e72ad4e2825f21ba4b101e16fc2db243150
Djingoo <56677342+Djingoo@users.noreply.github.com>
2023-04-13 17:24:29 +0200

    End Panel temporary fix

--- Main.gd
+++ Main.gd
@@ -245,11 +245,11 @@ func init_check_if_end_phase():
 
 
 func end():
-	#ErrorComponent.end.show()
-	#var scores = str("PARTIE FINIE \n\nScore Belgique : ", score_belgique, "\nScore Allemagne : ", score_allemagne, "\nScore Hollande : ", score_hollande, "\nScore Italie : ", score_italie)
+	ErrorComponent.end.show()
+	var scores = str("PARTIE FINIE \n\nScore Belgique : ", score_belgique, "\nScore Allemagne : ", score_allemagne, "\nScore Hollande : ", score_hollande, "\nScore Italie : ", score_italie)
 	$ErrorComponent/end/AnimationPlayer.play("popup")
-	#$ErrorComponent/end.text = scores
-	#print("end")
+	$ErrorComponent/end.dialog_text = scores
+	print("end")
 	#print("score_belgique += value", score_belgique)
 	#print("score_allemagne", score_allemagne)
 	#print("score_hollande", score_hollande)
