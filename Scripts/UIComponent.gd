9fdb22348ce81d733474a28321c503833dacba19
Djingoo <56677342+Djingoo@users.noreply.github.com>
2023-05-01 21:51:16 +0200

    images + chat update
    - buttons to choose cyclist now has image
    - every country has his own image
    - font color of the chat has been updated

--- Scripts/UIComponent.gd
+++ Scripts/UIComponent.gd
@@ -14,8 +14,6 @@ func display_deck_button(_team_deck: Array) -> void:
 	for carte in _team_deck:
 		var button = TextureButton.new()
 		button.rect_min_size.x = 60
-
-
 		
 		var image_path : String = ("res://Picture/Cards/" + str(carte) + ".png")
 		var image_selected_path : String = ("res://Picture/Cards/" + str(carte) + "_selected.png")
@@ -33,8 +31,6 @@ func display_deck_button(_team_deck: Array) -> void:
 		i += 1
 
 func display_team_deck(_team_deck: Array,team: int):
-	
-	
 	
 	var place
 	
@@ -64,9 +60,10 @@ func choose_player(value: int, index: int, cyclistes_movable: Array):
 	
 	for cycliste in cyclistes_movable :
 		var button = Button.new()
-		button.text = str(cycliste.name)
-		button.rect_min_size.x = 96
-		button.icon = preload("res://icon.png")
+		button.rect_min_size.x = 110
+		button.rect_min_size.y = 110
+		button.icon = load("res://Picture/Cyclistes/button_" + str(cycliste.name) + ".png")
+		button.expand_icon = true
 		choose_player_panel.add_child(button)
 		var _c = button.connect("pressed", get_parent(), "_button_player_pressed", [cycliste, value, index])
 
